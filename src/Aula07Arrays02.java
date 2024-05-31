f078d4e57203de26bff82ee93d6875718e535abb
EnzoBarbi <enzofagundes123@gmail.com>
2024-05-31 15:36:35 -0300

    adicionando aulas de java

---
+++ src/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+public class Aula07Arrays02 {
+
+    public static void main(String[] args) {
+        String[] nomes = new String[4];
+        nomes[0] = "Luffy";
+        nomes[1] = "Zoro";
+        nomes[2] = "Sanji";
+        nomes[3] = "Nami";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
+    }
+}
---
+++ src/OlaDevEnzo.java
@@ -0,0 +1,5 @@
+public class OlaDevEnzo {
+    public static void main(String[] args) {
+        System.out.println("Ola Dev Enzo");
+    }
+}
